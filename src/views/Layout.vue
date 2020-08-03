0360c9a7b0fe8d33c3e14f3226311d59687e5dd7
chf <chf0414@outlook.com>
2020-08-04 00:52:39 +0800

    init

---
+++ src/views/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Bar />
+    <Menu />
+  </div>
+</template>
+
+<script>
+import Bar from "../components/layout/Bar.vue";
+import Menu from "../components/layout/Menu.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Bar,
+    Menu
+  }
+};
+</script>
